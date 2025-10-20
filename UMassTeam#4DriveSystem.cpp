7b0890344eaa5c548fc71635d5e6a608c392cb20
roover04 <roover04@outlook.com>
2025-10-19 22:31:52 -0400

    Update UMassTeam#4DriveSystem.cpp

--- UMassTeam#4DriveSystem.cpp
+++ UMassTeam#4DriveSystem.cpp
@@ -5,30 +5,47 @@ Program disc: Drive System
 Contributors: 
 Ryder Overstreet: rooverstreet@umas.edu
 
+
+TO DO:
+- add a more intricate system for controls i.e. smoother transition into speed
+and better turning controls allowing for faster turning at more drastic inputs
+of the controls.
 */
 
-const int DIR = 18; // number = pin wire is connected to 
-const int STEP = 19;
+// stepper right 
+const int DIR_R = 18; // number = pin wire is connected to 
+const int STEP_R = 19;
+// controls
 const int VERT = 33;
 const int HORZ = 32;
 const int SEL = 27;
-const int DEAD = 200; // constant for 
+const int DEAD = 200; // constant for deadzone 
+// switch
+const int SWITCH = 35; 
+// stepper left 
+const int DIR_L = 25;
+const int STEP_L = 26;
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(STEP, OUTPUT); // STEP pin controls motor steps
-  pinMode(DIR, OUTPUT);  // DIR pin sets motor rotation direction
+  pinMode(STEP_R, OUTPUT); // STEP pin controls motor steps
+  pinMode(DIR_R, OUTPUT);  // DIR pin sets motor rotation direction
   pinMode(VERT, INPUT);  // vert axis input
   pinMode(HORZ, INPUT); // hoz axis input 
   //pinMode(SEL_PIN, INPUT_PULLUP); set SEL pin as input (allows for push button on joystick)
+  pinMode(SWITCH, HIGH); // switch input 
+  pinMode(STEP_L, OUTPUT);
+  pinMode(DIR_L, OUTPUT);
 }
 
 void stepMotor() 
 {
-  digitalWrite(STEP, LOW); 
+  digitalWrite(STEP_R, LOW); 
+  digitalWrite(STEP_L, LOW);
   delayMicroseconds(500);
-  digitalWrite(STEP, HIGH);
+  digitalWrite(STEP_R, HIGH);
+  digitalWrite(STEP_L, HIGH);
   delayMicroseconds(500);
 }
 
@@ -36,15 +53,37 @@ void loop()
 {
   int vert = analogRead(VERT); // get vert direction for controls
   int horz = analogRead(HORZ); // get horz direction for controls
+  int ks = analogRead(SWITCH); // get input of switch
 
+  if (ks == LOW)
+  {
+    return;
+  }
+
+  if (horz > 2048 + DEAD)
+  {
+    digitalWrite(DIR_R, HIGH);
+    digitalWrite(DIR_L, LOW);
+    stepMotor();
+  }
+  else if (horz < 2048 - DEAD)
+  {
+    digitalWrite(DIR_L, HIGH);
+    digitalWrite(DIR_R, LOW);
+    stepMotor();
+  }
+
+// forward and reverse controls 
   if (vert > 2048 + DEAD) // 2048 is half the analog range 
   {
-    digitalWrite(DIR, HIGH); // forward direction
+    digitalWrite(DIR_R, HIGH); // forward direction
+    digitalWrite(DIR_L, HIGH);
     stepMotor(); // while pushig up repeat function
   }
   else if (vert < 2048 - DEAD) 
   {
-    digitalWrite(DIR, LOW); // reverse direction
+    digitalWrite(DIR_R, LOW); // reverse direction
+    digitalWrite(DIR_L, LOW);
     stepMotor(); //while pulling back repeat function
   }
 }
